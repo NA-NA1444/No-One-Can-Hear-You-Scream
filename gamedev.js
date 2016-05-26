8cbd514ae5068fe0c690fa1cf3a1ff7c96146cb8
NA-NA1444 <NA-NA1444>
2016-05-26 18:58:04 +0800

    we added the monster

--- gamedev.js
+++ gamedev.js
@@ -1,42 +1,79 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
+$( document ).ready(function() {
 $("#startingbutton").click(function() {
   $("#back2").show({});
     $("#startingbutton").hide({});
   $("#startingb").hide({});
+    $("#instructions").hide({});
     $("#right1").show({});
   $("#left1").show({});
     $("#forward").show({});
       $("#h1").fadeIn({});
-        $(".radial-timer s-animate").fadeIn({});
-  $(".radial-timer-half").fadeIn({});
-  $(".radial-timer-half").fadeIn({});
-
-
 });
 
 $("#back2").hide({});
-  $("#backthree").hide({});
+  $("#back3three").hide({});
   $("#right1").hide({});
   $("#left1").hide({});
   $("#forward").hide({});
   $("#h1").hide({});
+  $("#hands2").hide({});
+  $("#forward2").hide({});
+  $("#back4").hide({});
+  $("#hands4").hide({});
+  $("#monster1").hide({});
+        $("#warning1").hide({});
+
+
 
 
 $("#right1").click(function() {
   $("#back2").fadeOut({});
-  $("#backthree").fadeIn({});
+  $("#back4").fadeIn({});
   $("#bman").fadeOut({});
   $("#right1").fadeOut({});
   $("#left1").fadeOut({});
-  $("#left3").fadeIn({});
     $("#forward").fadeOut({});
+      $("#hands").fadeOut({});
+  $("#hands4").fadeIn({});
+
+
+
 });
 
-$("#forward").click(function(){
+$("#forward").click(function() {
     $("#back3three").show({});
       $("#back2").hide({});
-  setTimeout(function(){ ("#monster1").show; }, 3000);
+  $("#backback").hide({});
+        $("#right1").hide({});
+        $("#left1").hide({});
+        $("#forward").hide({});
+        $("#hands").hide({});
+        $("#hands2").show({});
+          $("#forward2").show({});
+  $( "#monster1" ).slideUp( 300 ).delay( 4000 ).show( 400 );
   });
+  $("#forward2").click(function(){
+    
+    });
+    $("#instructions").animate({
+        left: '20px',
+         height: '+=10px',
+        width: '+=150px',
+        top: '600px',
+    });
+  });
+    
+  $(function(){
+	$("#hms_timer").countdowntimer({
+                hours : 3,
+		minutes : 10,
+                seconds : 10,
+                size : "lg",
+	});
+});
+    
+  
 });
