4e211be1151e10fbf94d9a8991fcd1af6d4a6365
NA-NA1444 <NA-NA1444>
2016-05-27 08:56:25 +0800

    end game

--- gamedev.js
+++ gamedev.js
@@ -1,23 +1,22 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
-$( document ).ready(function() {
-$("#startingbutton").click(function() {
-  $("#back2").show({});
+$(document).ready(function() {
+  $("#startingbutton").click(function() {
+    $("#back2").show({});
     $("#mask").show({});
     $("#startingbutton").hide({});
-  $("#startingb").hide({});
+    $("#startingb").hide({});
     $("#instructions").hide({});
     $("#right1").show({});
-  $("#left1").show({});
+    $("#left1").show({});
     $("#forward").show({});
-      $("#h1").fadeIn({});
-        $("#hands").fadeIn({});
-//starting button//
+    $("#h1").fadeIn({});
+    $("#hands").fadeIn({});
 
-});
+  });
 
-$("#back2").hide({});
+  $("#back2").hide({});
   $("#back3three").hide({});
   $("#right1").hide({});
   $("#left1").hide({});
@@ -28,86 +27,98 @@ $("#back2").hide({});
   $("#back4").hide({});
   $("#hands4").hide({});
   $("#monster1").hide({});
-        $("#warning1").hide({});
-        $("#mask").hide({});
-        $("#hands").hide({});
-    $("#mask2").hide({});
-//all hidden var/
+  $("#warning1").hide({});
+  $("#mask").hide({});
+  $("#hands").hide({});
+  $("#mask2").hide({});
+  $("#back5").hide({});
+    $("#gameover1").hide({});
+    $("#theend").hide({});
 
-$("#right1").click(function() {
-  $("#back2").fadeOut({});
-  $("#back4").fadeIn({});
-  $("#bman").fadeOut({});
-  $("#right1").fadeOut({});
-  $("#left1").fadeOut({});
+
+
+
+
+  $("#right1").click(function() {
+    $("#back2").fadeOut({});
+    $("#back4").fadeIn({});
+    $("#bman").fadeOut({});
+    $("#right1").fadeOut({});
+    $("#left1").fadeOut({});
     $("#forward").fadeOut({});
-      $("#hands").fadeOut({});
-  $("#hands4").fadeIn({});
+    $("#hands").fadeOut({});
+    $("#hands4").fadeIn({});
 
-//first right button//
+  });
 
+  $("#forward").click(function() {
+    $("#back3three").show({});
+    $("#back2").hide({});
+    $("#backback").hide({});
+    $("#right1").hide({});
+    $("#left1").hide({});
+    $("#forward").hide({});
+    $("#hands").hide({});
+    $("#hands2").show({});
+    $("#mask2").show({});
+    $("#warning1").slideUp(200).delay(800).show(300);
+    $("#warning1").slideDown(200).delay(800).hide(300);
+    $("#forward2").show({});
+    $("#monster1").slideUp(200).delay(4000).show(1000);
+    $("#gameover1").slideUp(200).delay(5200).show(3000);
+    $("#theend").slideUp(210).delay(5300).show(3100);
+  });
+  });
+
+
+$("#instructions").animate({
+  left: '20px',
+  height: '+=10px',
+  width: '+=150px',
+  top: '600px',
 });
 
-$("#forward").click(function() {
-    $("#back3three").show({});
-      $("#back2").hide({});
-  $("#backback").hide({});
-        $("#right1").hide({});
-        $("#left1").hide({});
-        $("#forward").hide({});
-        $("#hands").hide({});
-        $("#hands2").show({});
-          $("#mask2").show({});
-  $( "#warning1" ).slideUp( 200 ).delay( 800 ).show( 300 );
-  $( "#warning1" ).slideDown( 200 ).delay( 800 ).hide( 300 );
-          $("#forward2").show({});
-  $( "#monster1" ).slideUp( 200 ).delay( 4000 ).show( 1000 );
+$(function() {
+  $("#hms_timer").countdowntimer({
+    hours: 3,
+    minutes: 10,
+    seconds: 10,
+    size: "lg",
   });
-  $("#forward2").click(function(){
-    });
-    });
-   //forward button//
-  
-    $("#instructions").animate({
-        left: '20px',
-         height: '+=10px',
-        width: '+=150px',
-        top: '600px',
-    });
-  
-    
-  $(function(){
-	$("#hms_timer").countdowntimer({
-                hours : 3,
-		minutes : 10,
-                seconds : 10,
-                size : "lg",
-	});
 });
 
 var $img = $('#monster1'),
-    scale = 1.1,
-    h = $img.height(),
-    sh = h*scale;
+  scale = 1.1,
+  h = $img.height(),
+  sh = h * scale;
 
-function scaleUp($elt)
-{
-    $elt.animate({height: sh}, function ()
-    {
-        scaleDown($elt);
-    });
+function scaleUp($elt) {
+  $elt.animate({
+    height: sh
+  }, function() {
+    scaleDown($elt);
+  });
 }
 
-function scaleDown($elt)
-{
-    $elt.animate({height: h}, function ()
-    {
-        scaleUp($elt);
-    });
+function scaleDown($elt) {
+  $elt.animate({
+    height: h
+  }, function() {
+    scaleUp($elt);
+  });
 }
 
 scaleUp($img);
 
+$("#forward2").click(function(){
+    $("#back3three").hide({});
+     $("#back5").show({});
+    $("#monster1").hide({});
+    $("#warning1").hide({});
+    $("#mask2").hide({});
+    $("#mask").hide({});
+    $("#forward2").hide({});
+  });
 
 
 });
